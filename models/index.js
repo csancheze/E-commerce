6dda20634172d6aa4b10112b0695c5b6a8ef1249
Cesar Sanchez <cesanchezesc@gmail.com>
2022-02-18 08:42:40 -0600

    Category route finished!

--- models/index.js
+++ models/index.js
@@ -1,16 +1,33 @@
-// import models
+
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreingKey: 'category_id'
+})
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+})
 
-// Products belongToMany Tags (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tag'
+});
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'Tag_product'
+});
 
 module.exports = {
   Product,
